7c5816cddd5bb2a437ea62d5ae6d228f344e3858
Alexa Heitzenrader <aheitzen1@gmail.com>
2018-01-02 17:15:56 -0800

    wheee

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage();
 });
+
+$(document).ready(function (){
+	$("#30").click(function (){
+    	$('html, body').animate({
+        	scrollTop: $("#thirty-days").offset().top
+        }, 1000);
+  	});
+	$("#about").click(function (){
+    	$('html, body').animate({
+        	scrollTop: $("#mission-statement").offset().top
+        }, 1000);
+  	});
+  	$("#movie").click(function (){
+    	$('html, body').animate({
+        	scrollTop: $("#movie-section").offset().top
+        }, 1000);
+  	});
+    $("#contact").click(function (){
+      $('html, body').animate({
+          scrollTop: $("#footer").offset().top
+        }, 1000);
+    });
+    $("#pricing").click(function (){
+      $('html, body').animate({
+          scrollTop: $("#pricing-header").offset().top
+        }, 1000);
+    });
+});
